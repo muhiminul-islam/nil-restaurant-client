7d9f7c38259ce85a98b9246f475f1371622b70d1
muhiminul-islam <islammuhiminul11@gmail.com>
2023-06-26 04:55:43 +0600

    banner section done with responsive

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home first</h2>
+            <Banner></Banner>
         </div>
     );
 };
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -6,17 +6,13 @@ const Navbar = () => {
         <li><a>Item 5555</a></li>
         <li>
             <a>Parent</a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
         </li>
         <li><a>Item 3</a></li>
     </>
 
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-30 max-w-screen-xl bg-opacity-30 bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
