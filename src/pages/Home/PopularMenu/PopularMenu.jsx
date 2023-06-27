ef1fa44c96c0587ba98547c1c4f3ec8d5e4b9d63
muhiminul-islam <islammuhiminul11@gmail.com>
2023-06-27 10:29:38 +0600

    create custom hook to load data from one component to share all component

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,19 +1,13 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
+import { Link } from "react-router-dom";
 
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([])
-
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular');
-                setMenu(popularItems)
-            })
-    }, [])
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
     return (
         <section className="mb-12">
             <SectionTitle
@@ -22,13 +16,15 @@ const PopularMenu = () => {
             ></SectionTitle>
             <div className="grid md:grid-cols-2 gap-10 mt-10">
                 {
-                    menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
                         key={item._id}
                         item={item}
                     ></MenuItem>)
                 }
             </div>
-            <button className="btn btn-outline mx-auto md:flex justify-center border-0 border-b-4 mt-8">View Full Menu</button>
+            <Link to="/menu">
+                <button className="btn btn-outline mx-auto md:flex justify-center border-0 border-b-4 mt-8">View Full Menu</button>
+            </Link>
         </section>
     );
 };
