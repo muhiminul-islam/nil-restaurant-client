dfc952ac312ed3721a8fb83d1f1669d13a5e3944
muhiminul-islam <islammuhiminul11@gmail.com>
2023-08-03 22:59:21 +0600

    cart item show with tanstack query refetch

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -84,8 +84,9 @@ const Login = () => {
                                 </label>
                                 <input onBlur={handleValidateCaptcha} type="text" name="captcha" placeholder="type the captcha above" className="input input-bordered" />
                             </div>
+                            {/* TODO: make the button for captcha */}
                             <div className="form-control mt-6">
-                                <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
+                                <input disabled={false} className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
                         <p><small>New here? <Link to="/signup">Create an account</Link></small></p>
