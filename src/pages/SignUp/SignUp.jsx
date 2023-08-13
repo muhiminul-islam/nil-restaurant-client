91f146e38a73759362d311ba5007751662099f8d
muhiminul-islam <islammuhiminul11@gmail.com>
2023-08-13 20:00:28 +0600

    sign up with google one time user create

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 
 const SignUp = () => {
@@ -12,29 +13,42 @@ const SignUp = () => {
     const navigate = useNavigate();
 
     const onSubmit = data => {
-        console.log(data);
         createUser(data.email, data.password)
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
                 updateUserProfile(data.name, data.photoURL)
-                .then(() => {
-                    console.log('user profile data updated');
-                    reset();
-                    Swal.fire({
-                        position: 'top-end',
-                        icon: 'success',
-                        title: 'User Created Successfully',
-                        showConfirmButton: false,
-                        timer: 1500
-                    })
+                    .then(() => {
+                        const savedUser = { name: data.name, email: data.email }
+                        fetch('http://localhost:5000/users', {
+                            method: 'POST',
+                            headers: {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(savedUser)
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                if (data.insertedId) {
+                                    reset();
+                                    Swal.fire({
+                                        position: 'top-end',
+                                        icon: 'success',
+                                        title: 'User Created Successfully',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    })
 
-                    navigate('/');
+                                    navigate('/');
+                                }
+                            })
 
 
-                }).catch((error) => {
-                    console.log(error)
-                });
+
+
+                    }).catch((error) => {
+                        console.log(error)
+                    });
 
 
 
@@ -107,6 +121,7 @@ const SignUp = () => {
                             </div>
                         </form>
                         <p><small>Already have an account? <Link to="/login">Please Login</Link></small></p>
+                        <SocialLogin></SocialLogin>
                     </div>
                 </div>
             </div>
