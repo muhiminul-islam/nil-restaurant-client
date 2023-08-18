6b0f942006fa2945e54c662815ca405c7ab2bf06
muhiminul-islam <islammuhiminul11@gmail.com>
2023-08-18 23:28:13 +0600

    admin add item with imgbb hosting without secure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,7 +6,7 @@ import useAuth from './useAuth';
 
 
 const useAxiosSecure = () => {
-  const { logOut } = useAuth; 
+  const { logOut } = useAuth(); 
   const navigate = useNavigate(); 
 
   const axiosSecure = axios.create({
